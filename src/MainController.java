26caf26f4e753a8836ef9fb0c5435385fcd49efb
Moppieee <troxisai@kgg-do.eu>
2023-04-18 15:12:23 +0200

    yes

---
+++ src/MainController.java
@@ -0,0 +1,15 @@
+public class MainController {
+
+    public static void main(String[] args) {
+        for(int i = 0; i < 5; i++){
+            System.out.println("ew");
+            if(i == 3){
+                System.out.println("r");
+            }
+        }
+
+    }
+    public static void printSomeNumbers(){
+
+    }
+}
