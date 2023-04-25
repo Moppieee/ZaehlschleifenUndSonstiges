0b468c822025f41fb1e5ef2b206466665949b904
troxisai <troxisai@kgg-do.eu>
2023-04-25 11:21:13 +0200

    eee

--- src/MainController.java
+++ src/MainController.java
@@ -1,15 +1,57 @@
 public class MainController {
 
     public static void main(String[] args) {
-        for(int i = 0; i < 5; i++){
-            System.out.println("ew");
-            if(i == 3){
-                System.out.println("r");
-            }
-        }
+
+
+
 
     }
     public static void printSomeNumbers(){
+        for (int i = 1; i <= 60; i++){
+            System.out.println(i);
+        }
 
+    }
+    public static void printNumbers(int start, int end){
+        for (int i = start; i <= end; i++){
+            System.out.println(i);
+        }
+
+    }
+
+    public static int sumUp(int start, int end){
+        int sum= 0;
+        for(int i = start; i<= end; i++){
+            sum = sum + i;
+        }
+        return sum;
+    }
+
+    public static int powerOfTwo(int p){
+        if(p< 0) return -1;
+
+        int result = 0;
+        for(int i = 0; i <p; i++){
+            result = result * 2;
+        }
+        return result;
+    }
+
+    public static int sumUpPowerOfTwo(int n){
+       int sum = 0;
+       for(int i = 0; i <= n; i++){
+           sum = sum + powerOfTwo(i);
+        }
+       return sum;
+    }
+
+    //Aufgabe 3:
+    public static void rectangleStars(int width, int height){
+        for(int i = 0; int i <= height; i++){
+            for(int j = 0; j <=width; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
